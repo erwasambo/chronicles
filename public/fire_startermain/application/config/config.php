36dc014b4f10f8a35b2260be443c4891e2405f64
Wasambo <erick@afb.com>
2015-06-09 18:03:42 +0300

    few changes

--- public/fire_startermain/application/config/config.php
+++ public/fire_startermain/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost:8889/ci3-fire-starter/htdocs/';
+$config['base_url'] = 'http://localhost:8889/fire_startermain/htdocs/';
 
 /*
 |--------------------------------------------------------------------------
